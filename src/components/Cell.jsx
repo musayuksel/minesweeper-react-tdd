5056082e64a3f16f3165a9c02a087900c2358cc6
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-16 17:32:05 +0100

    Add reset functionality

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import './Cell.style.css';
 
-export default function Cell({ cell }) {
-    const classes = `cell ${
-      cell === 'X'
-        ? 'bomb'
-        : cell === '0'
-        ? 'empty'
-        : cell === '1'
-        ? 'one'
-        : cell === '2'
-        ? 'two'
-        : 'threeOrMore'
-    }`;
-    
-    return <div className={classes}>{cell}</div>;
+export default function Cell({ cell, isGameOver, setIsGameOver }) {
+  const [isRevealed, setIsRevealed] = React.useState(isGameOver || false);
+
+  React.useEffect(() => {
+    setIsRevealed(isGameOver);
+  }, [isGameOver]);
+
+  const handleClick = () => {
+    if (cell === 'X') {
+      setIsGameOver(true);
+    }
+    setIsRevealed(true);
+  };
+  const classes = `cell ${
+    !isRevealed
+      ? 'hidden'
+      : cell === 'X'
+      ? 'bomb'
+      : cell === '0'
+      ? 'empty'
+      : cell === '1'
+      ? 'one'
+      : cell === '2'
+      ? 'two'
+      : 'threeOrMore'
+  }`;
+
+  return (
+    <div onClick={handleClick} className={classes}>
+      {isRevealed ? cell : '?'}
+    </div>
+  );
 }
