501e17fadcf1e26e7794fe00e80deb209e07239b
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-16 18:03:29 +0100

    Refactor functions

--- src/utils/minesweeper.js
+++ src/utils/minesweeper.js
@@ -1,22 +1,59 @@
-export const boardWith = (chars) => Math.sqrt(chars.length);//N*N board
+export const boardWith = (chars) => Math.sqrt(chars.length); //N*N board
+
+export const upCellIndex = (currentCellIndex, boardWidth) =>
+  currentCellIndex - boardWidth;
+
+export const downCellIndex = (currentCellIndex, boardWidth) =>
+  currentCellIndex + boardWidth;
+
+export const leftCellIndex = (currentCellIndex, boardWidth) =>
+  currentCellIndex % boardWidth === 0 ? undefined : currentCellIndex - 1; //prevent left edge
+
+export const rightCellIndex = (currentCellIndex, boardWidth) =>
+  (currentCellIndex + 1) % boardWidth === 0 ? undefined : currentCellIndex + 1; //prevent right edge
+
+export const upLeftCellIndex = (currentCellIndex, boardWidth) => {
+  const upCell = upCellIndex(currentCellIndex, boardWidth);
+  return leftCellIndex(upCell, boardWidth);
+};
+
+export const upRightCellIndex = (currentCellIndex, boardWidth) => {
+  const upCell = upCellIndex(currentCellIndex, boardWidth);
+  return rightCellIndex(upCell, boardWidth);
+};
+
+export const downLeftCellIndex = (currentCellIndex, boardWidth) => {
+  const downCell = downCellIndex(currentCellIndex, boardWidth);
+  return leftCellIndex(downCell, boardWidth);
+};
+
+export const downRightCellIndex = (currentCellIndex, boardWidth) => {
+  const downCell = downCellIndex(currentCellIndex, boardWidth);
+  return rightCellIndex(downCell, boardWidth);
+};
 
 export const countMinesAroundCell = (board, currentCellIndex, boardWidth) => {
-    const upCellIndex = currentCellIndex - boardWidth;
-    const downCellIndex = currentCellIndex + boardWidth;
-    const leftCellIndex = (currentIndex) => currentIndex % boardWidth === 0 ? undefined : currentIndex - 1;//prevent left edge
-    const rightCellIndex = (currentIndex) => (currentIndex + 1) % boardWidth === 0 ? undefined : currentIndex + 1;//prevent right edge
-    const upLeftCellIndex = leftCellIndex(upCellIndex);
-    const upRightCellIndex = rightCellIndex(upCellIndex);
-    const bottomLeftCellIndex = leftCellIndex(downCellIndex);
-    const bottomRightCellIndex = rightCellIndex(downCellIndex);
+  const aroundOfCell = [
+    upCellIndex(currentCellIndex, boardWidth),
+    downCellIndex(currentCellIndex, boardWidth),
+    leftCellIndex(currentCellIndex, boardWidth),
+    rightCellIndex(currentCellIndex, boardWidth),
+    upLeftCellIndex(currentCellIndex, boardWidth),
+    upRightCellIndex(currentCellIndex, boardWidth),
+    downLeftCellIndex(currentCellIndex, boardWidth),
+    downRightCellIndex(currentCellIndex, boardWidth),
+  ];
 
-    const aroundOfCell = [upCellIndex, downCellIndex, leftCellIndex(currentCellIndex), rightCellIndex(currentCellIndex), upLeftCellIndex, upRightCellIndex, bottomLeftCellIndex, bottomRightCellIndex];
-
-    const mineCounter  = aroundOfCell.reduce((counter, cell) => board[cell] === 'X' ? counter + 1 : counter, 0);
-    return mineCounter.toString();
-}
+  const mineCounter = aroundOfCell.reduce(
+    (counter, cell) => (board[cell] === 'X' ? counter + 1 : counter),
+    0
+  );
+  return mineCounter.toString();
+};
 
 export const minesweeper = (chars) => {
-    const boardWidth = boardWith(chars);
-    return chars.map((char, index) => char === 'X' ? char : countMinesAroundCell(chars, index, boardWidth));
-}
+  const boardWidth = boardWith(chars);
+  return chars.map((char, index) =>
+    char === 'X' ? char : countMinesAroundCell(chars, index, boardWidth)
+  );
+};
