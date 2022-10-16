d7ce608c0dd2c4215387c37dd76e7f84e0db316e
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-16 17:31:45 +0100

    Add reset button

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,17 +3,27 @@ import { boardWith, minesweeper } from '../utils/minesweeper'
 import { boardStyles } from './Board.styles';
 import Cell from './Cell';
 
-export default function Board({ currentBoard = [] }) {
+export default function Board({ currentBoard ,resetBoard }) {
   const boardWidth = boardWith(currentBoard);
   const minesweeperBoard = minesweeper(currentBoard);
+  const [isGameOver, setIsGameOver] = React.useState(false);
   
   const board = minesweeperBoard.map((cell, index) => {
-    return <Cell key={index} cell={cell} />
+    return <Cell isGameOver={isGameOver} setIsGameOver={setIsGameOver} key={index} cell={cell} />
   });
 
   return (
+    <>
+      {isGameOver && 
+      <>
+      <h2>:(</h2>
+      <button onClick={()=>setIsGameOver(false)}>Try again with the same board</button>
+      <button onClick={()=>{resetBoard();  setIsGameOver(false)}}>Try again with a new board</button>
+      </>
+      }
     <section style={boardStyles(boardWidth)} className='boardContainer'>
       {board}
     </section>
+    </>
   )
 }
