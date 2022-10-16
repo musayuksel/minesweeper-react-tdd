4e66ede3b18655720c22fb16ab2f432049165237
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-16 12:48:03 +0100

    Add test for countMinesAroundCell

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { boardWith } from './utils/minesweeper';
+import { boardWith, countMinesAroundCell } from './utils/minesweeper';
 
 const board = [
   "-", "-", "-", "-", "-",
@@ -13,5 +13,18 @@ describe('Mine Sweeper', () => {
   it('should calculate the board width correctly', () => {
     const width = boardWith(board);
     expect(width).toBe(5);
+  });
+  it('should correctly count the mines around the cell', () => {
+    const topLeftCorner = countMinesAroundCell(board, 0, 5);
+    expect(topLeftCorner).toBe(0);
+
+    const secondRowLeftCorner = countMinesAroundCell(board, 5, 5);
+    expect(secondRowLeftCorner).toBe(2);
+
+    const topRightCorner = countMinesAroundCell(board, 4, 5);
+    expect(topRightCorner).toBe(0);
+
+    const forthRowRightCorner = countMinesAroundCell(board, 19, 5);
+    expect(forthRowRightCorner).toBe(1);
   });
 });
