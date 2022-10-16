61ad02ac82c456566ab50b96211c4ef089c6c851
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-16 13:05:48 +0100

    Fix tests return values

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { boardWith, countMinesAroundCell } from './utils/minesweeper';
+import { boardWith, countMinesAroundCell, minesweeper } from './utils/minesweeper';
 
 const board = [
   "-", "-", "-", "-", "-",
@@ -9,22 +9,37 @@ const board = [
   "-", "-", "-", "-", "-",
   "-", "-", "-", "-", "X",
 ];
+const expectedBoard = [
+  "0", "0", "0", "0", "0",
+  "2", "2", "1", "0", "0",
+  "X", "X", "1", "0", "0",
+  "2", "2", "1", "1", "1",
+  "0", "0", "0", "1", "X", 
+]
+
 describe('Mine Sweeper', () => {
   it('should calculate the board width correctly', () => {
     const width = boardWith(board);
     expect(width).toBe(5);
   });
+
   it('should correctly count the mines around the cell', () => {
     const topLeftCorner = countMinesAroundCell(board, 0, 5);
-    expect(topLeftCorner).toBe(0);
+    expect(topLeftCorner).toBe('0');
 
     const secondRowLeftCorner = countMinesAroundCell(board, 5, 5);
-    expect(secondRowLeftCorner).toBe(2);
+    expect(secondRowLeftCorner).toBe('2');
 
     const topRightCorner = countMinesAroundCell(board, 4, 5);
-    expect(topRightCorner).toBe(0);
+    expect(topRightCorner).toBe('0');
 
     const forthRowRightCorner = countMinesAroundCell(board, 19, 5);
-    expect(forthRowRightCorner).toBe(1);
+    expect(forthRowRightCorner).toBe('1');
   });
+
+  it('should render the board correctly', () => {
+    const boardResult = minesweeper(board);
+    expect(boardResult).toEqual(expectedBoard);
+  });
+
 });
