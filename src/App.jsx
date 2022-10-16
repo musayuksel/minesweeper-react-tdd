61837f9fc058b7695d9e3a0d11536dfc40132654
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-16 17:31:32 +0100

    Add styles and reusable functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
+import React from 'react';
 import './App.css';
 import Board from './components/Board';
-import { minesweeper } from './utils/minesweeper';
+import { prepareBoard } from './utils/prepareBoard';
 function App() {
 
-  const currentBoard =(([
-    "-", "-", "-", "-", "-",
-    "-", "-", "-", "-", "-",
-    "X", "X", "-", "-", "-",
-    "-", "-", "-", "-", "-",
-    "-", "-", "-", "-", "X", 
-]));
+  const [selectedLevel, setSelectedLevel] = React.useState(0);
+  const [currentBoard, setCurrentBoard] = React.useState([]);
+
+  React.useEffect(() => {
+    resetBoard();
+    // eslint-disable-next-line
+  }, [selectedLevel]);
+
+  function resetBoard() {
+    setCurrentBoard(prepareBoard(selectedLevel));
+  }
+  function handleLevelChange(width) {
+    setSelectedLevel(width);
+  }
+
   return (
     <div className="App">
-      Hello world
-      <Board currentBoard={currentBoard}/>
+      <h2>
+      Welcome to Minesweeper!
+      </h2>
+      <section className='levelContainer'>
+        <button onClick={()=>handleLevelChange(5)}>Beginner(5*5)</button>
+        <button onClick={()=>handleLevelChange(10)}>Intermediate(10*10)</button>
+        <button onClick={()=>handleLevelChange(20)}>Expert(20*20)</button>
+        <button >Add your custom Mines</button>
+      </section>
+      <Board currentBoard={currentBoard} resetBoard={resetBoard}/>
     </div>
   );
 }
